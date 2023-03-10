6ef1779669141495193927fa60ed04af533278cc
Valerija Hodolič <xhodolic@stuba.sk>
2023-03-11 00:18:07 +0100

    Vytvorenie triedy Student a jej vsetkych atributoch,getter,setter,constructor

---
+++ src/main/java/sk/stuba/fei/uim/oop/Student.java
@@ -0,0 +1,47 @@
+package sk.stuba.fei.uim.oop;
+
+public class Student {
+    int ID;
+    String meno;
+    String priezvisko;
+    int vek;
+
+    public Student(int ID, String meno, String priezvisko, int vek) {
+        this.ID = ID;
+        this.meno = meno;
+        this.priezvisko = priezvisko;
+        this.vek = vek;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public void setMeno(String meno) {
+        this.meno = meno;
+    }
+
+    public void setPriezvisko(String priezvisko) {
+        this.priezvisko = priezvisko;
+    }
+
+    public void setVek(int vek) {
+        this.vek = vek;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getMeno() {
+        return meno;
+    }
+
+    public String getPriezvisko() {
+        return priezvisko;
+    }
+
+    public int getVek() {
+        return vek;
+    }
+}
